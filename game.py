7e4f6e1c0b58d03b1655b7469abc50694ad5362e
Yang Guo <yguo57@my.bcit.ca>
2023-11-15 23:35:26 -0800

    Complete functions for generating and printing game map

--- game.py
+++ game.py
@@ -53,17 +53,19 @@ def ask_questionnaire():
 
     return answers
 
+
 def print_stats():
     """
     prints character stats to screen
     """
+
 
 def make_board(row, column):
     game_board = {}
     locations = {
         (3, 4): "school",
         (5, 8): "hospital",
-        (9, 8): "park",
+        (2, 12): "park",
         (7, 3): "work",
     }
     for x in range(row):
@@ -76,14 +78,49 @@ def make_board(row, column):
     return game_board
 
 
-def generate_map(location):
-    # location = city, park, school
-    pass
+def generate_game_map(game_board):
+    for (x, y), location in game_board.items():
+        if location == "hospital":
+            game_board[(x, y)] = 'H'
+            update_surroundings(game_board, x, y, '|', '+', 'H')
+        elif location == "school":
+            game_board[(x, y)] = 'S'
+            update_surroundings(game_board, x, y, '|', '-', 'S')
+        elif location == "park":
+            game_board[(x, y)] = 'P'
+            update_surroundings(game_board, x, y, '=', '=', 'P')
+        elif location == "work":
+            game_board[(x, y)] = 'W'
+            update_surroundings(game_board, x, y, '|', '~', 'W')
+
+    return game_board
 
 
-def print_current_map(location):
-    # prints map of player's current location to screen
-    pass
+def update_surroundings(game_board, x, y, horizontal_symbol, vertical_symbol, location_symbol):
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if (i, j) in game_board and game_board[(i, j)] != location_symbol:
+                game_board[(i, j)] = horizontal_symbol
+
+    for j in range(y - 1, y + 2):
+        for i in [x - 1, x + 1]:
+            if (i, j) in game_board and game_board[(i, j)] != location_symbol:
+                game_board[(i, j)] = vertical_symbol
+
+
+def print_map(game_board, row, column):
+    for i in range(row):
+        for j in range(column):
+            if i == 0 or i == row - 1:
+                if j == 0 or j == column - 1:
+                    print('+', end=' ')
+                else:
+                    print('-', end=' ')
+            elif j == 0 or j == column - 1:
+                print('|', end=' ')
+            else:
+                print(game_board[(i, j)], end=' ')
+        print()
 
 
 def check_fast_travel(location):
@@ -265,7 +302,11 @@ def run_weekend():
 def game():
     player = greeting()
     print(player)
-
+    rows = 10
+    columns = 18
+    board = make_board(rows, columns)
+    game_map = generate_game_map(board)
+    print_map(game_map, rows, columns)
 
 if __name__ == '__main__':
     game()
