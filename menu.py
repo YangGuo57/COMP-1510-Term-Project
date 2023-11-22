230d19794d43c88d8d8cd942a283c9b2ca7c8a1c
Yang Guo <yguo57@my.bcit.ca>
2023-11-22 01:09:14 -0800

    Add home menu

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import map as mp
 import movement as mov
-import create_character as cha
+import character as cha
 import school_map as smp
-import event_trigger as e
+import random
 
 
 def main_menu(character):
@@ -34,6 +34,21 @@ def school_menu(character):
         choice = input("Please choose an option: ")
         if choice == '1':
             smp.school_map_action(character)
+        elif choice == '2':
+            mp.map_action(character)
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
+
+def home_menu(character):
+    while True:
+        print("1. Enter home")
+        print("2. Leave home")
+
+        choice = input("Please choose an option: ")
+        if choice == '1':
+            cha.change_stat(character, 'stress', random.randint(-20, -15))
         elif choice == '2':
             mp.map_action(character)
             break
