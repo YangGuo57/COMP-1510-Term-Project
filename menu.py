deea8f5c10a26632d407f0eb204f591092c4e350
Yang Guo <yguo57@my.bcit.ca>
2023-11-21 22:27:03 -0800

    Create school map

--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
 import map as mp
 import movement as mov
 import create_character as cha
+import school_map as smp
+import event_trigger as e
 
 
 def main_menu(character):
@@ -22,4 +24,18 @@ def main_menu(character):
             break
         else:
             print("Invalid choice. Please enter a valid option.")
-        return choice
+
+
+def school_menu(character):
+    while True:
+        print("1. Enter School")
+        print("2. Leave School")
+
+        choice = input("Please choose an option: ")
+        if choice == '1':
+            smp.school_map_action(character)
+        elif choice == '2':
+            mp.map_action(character)
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
