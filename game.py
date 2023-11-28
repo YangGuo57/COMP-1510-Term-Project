8164a9fc9f346498fabfc48514fcb4754b536087
Yang Guo <yguo57@my.bcit.ca>
2023-11-27 19:48:28 -0800

    Add save and load game

--- game.py
+++ game.py
@@ -2,37 +2,80 @@ import map
 import character
 import weekday
 import weekend
+import json
+import utils
 
 
-def greeting():
-    greeting_dict = {
-        1: "Welcome to Survive CST. Please take a moment to answer the following questionnaire. There is no right"
-           " or wrong answer.",
-        2: "It is the first week of September, your first time in the City of Vancouver, and your first day studying. "
-           "Computer Systems Technology (CST) at BCIT. You enter the school elevator with mixed feelings; after all, "
-           "you came to this foreign city in hopes that computer science is the right career for you. As you step out "
-           "of the elevator, you tell yourself: Don't stress, learn lots, make friends, get co-op, and most "
-           "importantly, have fun!",
-        3: "",
+def save_game(player, week, is_weekend, filename='game_save.json'):
+    game_data = {
+        'character': player,
+        'week': week,
+        'is_weekend': is_weekend,
     }
+    with open(filename, 'w') as file:
+        json.dump(game_data, file, indent=4)
 
-    return greeting_dict
+
+def load_game(filename='game_save.json'):
+    try:
+        with open(filename, 'r') as file:
+            game_data = json.load(file)
+        return game_data['character'], game_data['week'], game_data.get('is_weekend', False)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading the game: {e}")
+        return None, None, False
 
 
-def game():
-    greeting_msg = greeting()
+def start_new_game(greeting_msg):
     print(greeting_msg[1])
     answer = character.ask_questionnaire()
     player = character.create_character(answer)
     print(greeting_msg[2])
+    week = 1
+    return player, week
+
+
+def game():
+    greeting_msg = utils.greeting()
+    print("Welcome to Survive CST!")
+    print("1. Start New Game")
+    print("2. Load Previous Game")
+    while True:
+        load_choice = input("Please choose an option (1 or 2): ")
+        if load_choice == '1':
+            print("Starting a fresh new journey in Survive CST!")
+            player, week = start_new_game(greeting_msg)
+            is_weekend = False
+            break
+        elif load_choice == '2':
+            player, week, is_weekend = load_game()
+            if player is None:
+                print("No previous save found. Starting a new adventure!")
+                player, week = start_new_game(greeting_msg)
+                is_weekend = False
+            break
+        else:
+            print("Invalid option. Please enter '1' for a new game or '2' to load a previous game.")
+
     main_map = map.initialize_map(10, 20, 'coordinates')
     school_map = map.initialize_map(13, 9, 'school')
 
-    for week in range(1, 8):
-        print(f"========== Week {week} ==========")
-        weekday.run_weekday(player, week, school_map)
-        weekend.run_weekend(player, main_map)
-        if week == 7:
+    for current_week in range(week, 8):
+        print(f"========== Week {current_week} ==========")
+        if not is_weekend:
+            weekday.run_weekday(player, current_week, school_map)
+            is_weekend = True
+
+        # Exit the game
+        if is_weekend:
+            weekend_result = weekend.run_weekend(player, main_map, current_week)
+            save_game(player, current_week, is_weekend)
+            if not weekend_result:
+                break
+            is_weekend = False
+
+        save_game(player, current_week, is_weekend)
+        if current_week == 7:
             print("Midterm exams are starting!")
 
 
