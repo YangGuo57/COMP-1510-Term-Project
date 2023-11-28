7fff650c030d5e7f269640c7d0cb90185db7db45
Yang Guo <yguo57@my.bcit.ca>
2023-11-27 18:47:01 -0800

    Group menu and greeting into utils

--- menu.py
+++
@@ -1,8 +0,0 @@
-def main_menu():
-    print("1. Move")
-    print("2. Check Status")
-    print("3. Fast Travel")
-    print("4. Exit")
-
-    choice = input("Please choose an option: ")
-    return choice
---
+++ utils.py
@@ -0,0 +1,23 @@
+def main_menu():
+    print("1. Move")
+    print("2. Check Status")
+    print("3. Fast Travel")
+    print("4. Exit")
+
+    choice = input("Please choose an option: ")
+    return choice
+
+
+def greeting():
+    greeting_dict = {
+        1: "Welcome to Survive CST. Please take a moment to answer the following questionnaire. There is no right"
+           " or wrong answer.",
+        2: "It is the first week of September, your first time in the City of Vancouver, and your first day studying. "
+           "Computer Systems Technology (CST) at BCIT. You enter the school elevator with mixed feelings; after all, "
+           "you came to this foreign city in hopes that computer science is the right career for you. As you step out "
+           "of the elevator, you tell yourself: Don't stress, learn lots, make friends, get co-op, and most "
+           "importantly, have fun!",
+        3: "",
+    }
+
+    return greeting_dict
