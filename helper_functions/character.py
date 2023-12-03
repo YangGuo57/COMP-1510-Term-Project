24a119400844d28f9c2daebbbfc8b6cfef2e02ea
Yang Guo <yguo57@my.bcit.ca>
2023-12-03 12:11:05 -0800

    Set default visited school at 1

--- helper_functions/character.py
+++ helper_functions/character.py
@@ -16,7 +16,7 @@ def create_character(answers):
     {'IQ': 1.0, 'EQ': 10, 'stress': 0, 'wealth': 40, 'X': 1, 'Y': 1, 'project': 0, 'exp': {'1510': \
 0, '1537': 0, '1113': 0, '1712': 0}, 'lvl': {'1510': 0, '1537': 0, '1113': 0, '1712': 0}, 'midterm': \
 {'1510': None, '1537': None, '1113': None, '1712': None}, 'final': {'1510': None, '1537': None, '1113': \
-None, '1712': None}, 'visited_locations': {'home': 1, 'school': 0, 'hospital': 0, 'park': 0, 'work': 0, \
+None, '1712': None}, 'visited_locations': {'home': 1, 'school': 1, 'hospital': 0, 'park': 0, 'work': 0, \
 '1510': 0, '1537': 0, '1712': 0, '1113': 0}, 'location': 'home', 'vaccinated': False, 'skip_job': 0}
     """
     new_character = {'IQ': 0, 'EQ': 0, 'stress': 0, 'wealth': 0, 'X': 1, 'Y': 1, 'project': 0,
@@ -24,7 +24,7 @@ None, '1712': None}, 'visited_locations': {'home': 1, 'school': 0, 'hospital': 0
                      'lvl': {'1510': 0, '1537': 0, '1113': 0, '1712': 0},
                      'midterm': {'1510': None, '1537': None, '1113': None, '1712': None},
                      'final': {'1510': None, '1537': None, '1113': None, '1712': None},
-                     'visited_locations': {'home': 1, 'school': 0, 'hospital': 0, 'park': 0, 'work': 0, "1510": 0,
+                     'visited_locations': {'home': 1, 'school': 1, 'hospital': 0, 'park': 0, 'work': 0, "1510": 0,
                                            "1537": 0, "1712": 0, "1113": 0},
                      'location': 'home', 'vaccinated': False, 'skip_job': 0}
 
