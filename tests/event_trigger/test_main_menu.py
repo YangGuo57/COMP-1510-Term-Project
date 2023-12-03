ceefad3e8394e7f795340ba8560f32b40d89c627
Yang Guo <yguo57@my.bcit.ca>
2023-12-03 11:23:40 -0800

    Complete unittest

---
+++ tests/event_trigger/test_main_menu.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from helper_functions.event_trigger import main_menu
+from unittest.mock import patch
+
+
+class Test(TestCase):
+    @patch('builtins.input', side_effect=['1', '2', '3', '4'])
+    def test_main_menu_valid_input(self, mock_input):
+        self.assertEqual(main_menu(), '1')
+        self.assertEqual(main_menu(), '2')
+        self.assertEqual(main_menu(), '3')
+        self.assertEqual(main_menu(), '4')
