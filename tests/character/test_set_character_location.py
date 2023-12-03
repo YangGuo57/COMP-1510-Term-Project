64b3340d89b8622afd50b225103ebfaa964a79b0
Yang Guo <yguo57@my.bcit.ca>
2023-12-02 17:56:14 -0800

    Complete unittest

---
+++ tests/character/test_set_character_location.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from helper_functions.character import set_character_location
+
+
+class Test(TestCase):
+    def test_set_character_location_empty_string(self):
+        character = {'location': 'home'}
+        set_character_location(character, '')
+        self.assertEqual(character['location'], '')
+
+    def test_set_character_location_successfully(self):
+        character = {'location': 'home'}
+        set_character_location(character, 'hospital')
+        self.assertEqual(character['location'], 'hospital')
