f439adc5230f07e072333f3fe7f043df54705b58
Yang Guo <yguo57@my.bcit.ca>
2023-12-03 00:35:37 -0800

    Complete unittest

---
+++ tests/character/test_ask_questionnaire.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest.mock import patch
+from helper_functions.character import ask_questionnaire
+
+
+class Test(TestCase):
+    @patch('helper_functions.character.input', side_effect=['1', '2', '1', '2'])
+    def test_ask_questionnaire_coop_setting(self, mock_input):
+        setting = 'coop'
+        expected_answers = [0, 1, 0, 1]
+        actual_answers = ask_questionnaire(setting)
+        self.assertEqual(actual_answers, expected_answers)
+        self.assertEqual(mock_input.call_count, 4)
+
+    @patch('helper_functions.character.input', side_effect=['2', '1', '2', '1'])
+    def test_ask_questionnaire_new_setting(self, mock_input):
+        setting = 'new'
+        expected_answers = [1, 0, 1, 0]
+        actual_answers = ask_questionnaire(setting)
+        self.assertEqual(actual_answers, expected_answers)
+        self.assertEqual(mock_input.call_count, 4)
