7ba7a495ef56c5b4f8493ef2d1842dacb0028eed
Yang Guo <yguo57@my.bcit.ca>
2023-12-03 14:47:08 -0800

    Complete unittest

---
+++ tests/event_trigger/test_at_entrance.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+from unittest.mock import patch
+from helper_functions.event_trigger import at_entrance
+
+
+class Test(TestCase):
+
+    def setUp(self):
+        self.mock_map_data = {
+            "door": {
+                "main": {"home": (2, 3),
+                         "school": (3, 9),
+                         "hospital": (6, 12),
+                         "park": (2, 15),
+                         "work": (7, 4)},
+                "school": {
+                    "1510": (2, 6),
+                    "1537": (10, 6),
+                    "1712": (7, 6),
+                    "1113": (4, 6),
+                }
+            }
+        }
+
+    @patch('helper_functions.map.coordinates')
+    def test_character_not_at_entrance(self, mock_coordinates):
+        mock_coordinates.return_value = self.mock_map_data
+        character = {'location': 'outside', 'X': 1, 'Y': 2}
+        self.assertIsNone(at_entrance(character))
+
+    @patch('helper_functions.map.coordinates')
+    def test_character_at_school_entrance_1537(self, mock_coordinates):
+        mock_coordinates.return_value = self.mock_map_data
+        character = {'location': 'school', 'X': 10, 'Y': 6}
+        self.assertEqual(at_entrance(character), '1537')
+
+    @patch('helper_functions.map.coordinates')
+    def test_character_not_matching_any_entrance(self, mock_coordinates):
+        mock_coordinates.return_value = self.mock_map_data
+        character = {'location': 'outside', 'X': 8, 'Y': 8}
+        self.assertIsNone(at_entrance(character))
+
+    @patch('helper_functions.map.coordinates')
+    def test_character_missing_coordinates(self, mock_coordinates):
+        mock_coordinates.return_value = self.mock_map_data
+        character = {'location': 'outside'}
+        with self.assertRaises(KeyError):
+            at_entrance(character)
