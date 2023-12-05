bef253d0832a63fee88ea15d3b34fdaf1a7849fe
Yang Guo <yguo57@my.bcit.ca>
2023-12-04 21:45:35 -0800

    Fix

--- tests/weekend/test_generate_job_posting.py
+++ tests/weekend/test_generate_job_posting.py
@@ -5,14 +5,14 @@ from helper_functions.weekend import generate_job_posting
 
 class Test(TestCase):
     @patch('helper_functions.weekend.binary_user_choice', return_value='1')
-    def test_character_applies_for_job(self, mock_choice):
+    def test_generate_job_posting_character_applies_for_job(self, mock_choice):
         character = {'job': False}
         result = generate_job_posting(character)
         self.assertTrue(result)
         self.assertTrue(character['job'])
 
     @patch('helper_functions.weekend.binary_user_choice', return_value='2')
-    def test_character_does_not_apply_for_job(self, mock_choice):
+    def test_generate_job_posting_character_does_not_apply_for_job(self, mock_choice):
         character = {'job': False}
         result = generate_job_posting(character)
         self.assertFalse(result)
