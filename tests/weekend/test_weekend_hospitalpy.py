e9af2ec06f7609e583df860eafec5ff26731839f
Yang Guo <yguo57@my.bcit.ca>
2023-12-06 09:52:54 -0800

    Complete unittest

---
+++ tests/weekend/test_weekend_hospitalpy.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from unittest.mock import patch
+from helper_functions.weekend import weekend_hospital
+
+
+class Test(TestCase):
+    def setUp(self):
+        self.character = {'IQ': 0, 'vaccinated': False}
+
+    @patch('builtins.print')
+    @patch('helper_functions.weekend.binary_user_choice', return_value='1')
+    def test_weekend_hospital_not_vaccinated(self, mock_user_choice, mock_print):
+        def mock_change_stat(character, key, value):
+            character[key] += value
+
+        with patch('helper_functions.character.change_stat', new=mock_change_stat):
+            weekend_hospital(self.character)
+        self.assertEqual(self.character['IQ'], 1)
+        self.assertTrue(self.character['vaccinated'])
+        mock_user_choice.assert_called_once_with('hospital')
+        mock_print.assert_called()
+
+    @patch('builtins.print')
+    def test_weekend_hospital_already_vaccinated(self, mock_print):
+        self.character['vaccinated'] = True
+        weekend_hospital(self.character)
+        self.assertEqual(self.character['IQ'], 0)
+        mock_print.assert_called()
