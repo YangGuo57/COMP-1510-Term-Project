3bafb28f5260c009b65159e81db5767d1a73dd49
Alice Huang <alice.in.realityland@gmail.com>
2023-12-06 16:27:24 -0800

    Fix type

--- helper_functions/__init__.py
+++ helper_functions/__init__.py
@@ -7,10 +7,10 @@ def greeting():
     greeting_dict = {
         1: "Welcome to Survive CST. Please take a moment to answer the following questionnaire. There is no right"
            " or wrong answer.",
-        2: "It is the first week of September, your first time in the City of Vancouver, and your first day studying. "
+        2: "It is the first week of September, your first time in the City of Vancouver, and your first day studying "
            "Computer Systems Technology (CST) at BCIT. You enter the school elevator with mixed feelings; after all, "
            "you came to this foreign city in hopes that computer science is the right career for you. As you step out "
-           "of the elevator, you tell yourself: Don't stress, learn lots, make friends, get co-op, and most "
+           "of the elevator, you tell yourself: don't stress, learn lots, make friends, get co-op, and most "
            "importantly, have fun!",
         3: "\nJust remember: do NOT fail any midterms or finals, because you'll get kicked out of the program!",
     }
