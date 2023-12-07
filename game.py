efa91a79134a52e5f5b620cb158badd65982157e
Alice Huang <alice.in.realityland@gmail.com>
2023-12-06 17:42:31 -0800

    Add docstring and merge functions

--- game.py
+++ game.py
@@ -3,30 +3,41 @@ from helper_functions import (weekday, exam, weekend, map, TOTAL_WEEKS,
 from time import sleep
 
 
-def determine_game_ending(player, coop):
-    gpa = exam.calculate_average(player)
-    endings = ending_descriptions()
-    if gpa is None:
-        return
-    elif coop:
-        print(f'{endings["coop"]}')
-    else:
-        print(f'{endings[gpa]}')
-
-
 def print_game_ending(player, test, pass_exam, pass_coop):
+    """
+    Prints the game ending including the ASCII art based on the player's GPA and whether they passed.
+    
+    :param player: a dictionary representing the player
+    :param test: a string representing the test the player took
+    :param pass_exam: a boolean representing whether the player passed the exam
+    :param pass_coop: a boolean representing whether the player passed the coop interview
+    :precondition: player must be a dictionary generated within this game
+    :precondition: test must be a string generated in this game
+    :postcondition: correctly prints the game ending based on the player's GPA and whether they passed
+    """
     print()
     if not pass_exam:
         print(f'YOU FAILED YOUR {test.upper()}S! YOU HAVE BEEN KICKED OUT OF THE CST PROGRAM :(')
         sleep(0.5)
         ascii.print_ascii('game over')
     else:
-        determine_game_ending(player, pass_coop)
+        # determine_game_ending(player, pass_coop)
+        gpa = exam.calculate_average(player)
+        endings = ending_descriptions()
+        if gpa is None:
+            return
+        elif pass_coop:
+            print(f'{endings["coop"]}')
+        else:
+            print(f'{endings[gpa]}')
         sleep(0.5)
         ascii.print_ascii('congratulations')
 
 
 def game():
+    """
+    Drive the game
+    """
     player, week, is_weekend = save.new_or_load()
     main_map = map.initialize_map(10, 20, 'coordinates')
     school_map = map.initialize_map(13, 9, 'school')
