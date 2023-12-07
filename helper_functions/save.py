d35c6127a4126f73cfd2c13bf1a010b9a5682971
Alice Huang <alice.in.realityland@gmail.com>
2023-12-06 18:02:47 -0800

    Add docstrings

--- helper_functions/save.py
+++ helper_functions/save.py
@@ -4,7 +4,11 @@ import json
 
 
 def new_or_load():
-    greeting_msg = greeting()
+    """
+    Gets user input on whether to start a new game or load a previous game.
+
+    :return: a tuple containing the player, week, and is_weekend
+    """
     sleep(0.5)
     print('Welcome to Survive CST!')
     print('1. Start New Game')
@@ -13,22 +17,28 @@ def new_or_load():
         load_choice = input('Please choose an option (1 or 2): ')
         if load_choice == '1':
             print('Starting a fresh new journey in Survive CST!')
-            player, week = start_new_game(greeting_msg)
+            player, week = start_new_game()
             is_weekend = False
         elif load_choice == '2':
             player, week, is_weekend = load_game()
             if player is None:
                 print('No previous saved_game found. Starting a new adventure!')
                 is_weekend = False
-                player, week = start_new_game(greeting_msg)
+                player, week = start_new_game()
         else:
             print("Invalid option. Please enter '1' for a new game or '2' to load a previous game.")
             continue
         return player, week, is_weekend
 
 
-def start_new_game(greeting_msg):
+def start_new_game():
+    """
+    Starts a new game by initializing a new character.
+
+    :return: a tuple containing the player and week
+    """
     sleep(0.5)
+    greeting_msg = greeting()
     print(greeting_msg[1])
     answer = character.ask_questionnaire('new')
     player = character.create_character(answer)
@@ -39,6 +49,14 @@ def start_new_game(greeting_msg):
 
 
 def load_game(filename='./saved_game.json'):
+    """
+    Loads a previous game from a JSON file.
+    
+    :param filename: a string representing the filename of the JSON file
+    :precondition: filename must be a relative path to a JSON file
+    :postcondition: correctly loads the game from the JSON file
+    :return: a tuple containing the player, week, and is_weekend
+    """
     try:
         with open(filename, 'r') as file:
             game_data = json.load(file)
@@ -49,6 +67,18 @@ def load_game(filename='./saved_game.json'):
 
 
 def save_game(player, week, is_weekend, filename='./saved_game.json'):
+    """
+    Saves the current game to a JSON file.
+    
+    :param player: a dictionary representing the player
+    :param week: an integer representing the current week
+    :param is_weekend: a boolean representing whether it is the weekend
+    :param filename: a string representing the relative path of the JSON file
+    :precondition: player must be a dictionary generated within this game
+    :precondition: week must be an integer generated in this game
+    :precondition: filename must be a relative path to a JSON file
+    :postcondition: correctly saves the game to the JSON file
+    """
     game_data = {
         'character': player,
         'week': week,
