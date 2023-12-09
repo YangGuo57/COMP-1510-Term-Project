70836933e629ead8492ec48006b152d8d8c17acb
Yang Guo <yguo57@my.bcit.ca>
2023-12-08 23:30:08 -0800

    saved_game pass unittest

--- tests/save/test_saved_game_file.py
+++ tests/save/test_saved_game_file.py
@@ -4,5 +4,6 @@ import pathlib
 
 class Test(TestCase):
     def test_file_exists(self):
-        path = pathlib.Path('../../saved_game.json')
+        dir_path = pathlib.Path(__file__).parent.resolve()
+        path = dir_path / '../../saved_game.json'
         self.assertTrue(path.is_file())
